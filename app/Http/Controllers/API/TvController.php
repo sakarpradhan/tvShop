3b4628b86b38d4fd8530b692e5661cc12d081466
sakar <sakarpradhansp@gmail.com>
2022-05-12 17:01:27 +0545

    add api for tv

---
+++ app/Http/Controllers/API/TvController.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Http\Controllers\API;
+
+use App\Http\Controllers\Controller;
+use App\Http\Requests\TVFormRequest;
+use App\Models\Tv;
+use Illuminate\Support\Facades\Storage;
+use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+
+class TvController extends Controller
+{
+    public function index()
+    {
+        $tvs = Tv::all();
+        foreach ($tvs as $tv) {
+            $tv['path'] = asset('storage/' . $tv['path']);
+        }
+        return response()->json($tvs);
+    }
+
+    public function show($tv)
+    {
+        try {
+            $tv = Tv::findOrFail($tv);
+            $tv['path'] = asset('storage/' . $tv['path']);
+            return response()->json($tv);
+        } catch (ModelNotFoundException $error) {
+            return response([
+                'status' => '404',
+                'error' => 'Record not found.'
+            ], 404);
+        }
+    }
+
+    public function store(TVFormRequest $request)
+    {
+        $attributes = $request->validated();
+
+        $attributes['path'] = request()->file('path')->store('tv_images');
+
+        $tv = Tv::create($attributes);
+
+        return response()->json($tv);
+    }
+
+    public function update(TVFormRequest $request, $tv)
+    {
+        $attributes = $request->validated();
+
+        if (isset($attributes['path'])) {
+            $attributes['path'] = request()->file('path')->store('tv_images');
+            $oldImg = Tv::where('id', $tv->id)->first()->path;
+            if (Storage::exists($oldImg)) {
+                Storage::delete($oldImg);
+            }
+        }
+
+        $tv->update($attributes);
+
+        return response()->json($tv);
+    }
+
+    public function destroy($tv)
+    {
+        $img = Tv::where('id', $tv)->first()->path;
+        if (Storage::exists($img)) Storage::delete($img);
+
+        Tv::where('id', $tv)->delete();
+        return response()->json(['message' => 'TV removed from database.'], 204);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\API\TvController;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +15,14 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//    return $request->user();
+//});
+
+Route::get('/tv', [TvController::class, 'index']);
+Route::get('/tv/{tv}', [TvController::class, 'show']);
+
+Route::post('/store', [TvController::class, 'store']);
+Route::put('/update/{tv}', [TvController::class, 'update']);
+
+Route::delete('/destroy/{tv}', [TvController::class, 'destroy']);
